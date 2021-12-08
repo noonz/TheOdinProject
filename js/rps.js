fd1dd786f998db1c934dc31dc39783898080737f
Dave Nunez <noonz2@icloud.com>
2021-12-08 00:21:43 -0500

    fix rps game

--- js/rps.js
+++ js/rps.js
@@ -1,6 +1,6 @@
 function playRound(playerSelection, computerSelection) {
     switch (playerSelection) {
-        case '1':
+        case 0:
             if (computerSelection === 'rock') {
                 return 'Draw!';
             } else if (computerSelection === 'paper') {
@@ -9,7 +9,7 @@ function playRound(playerSelection, computerSelection) {
                 return 'You win!';
             }
             break
-        case '2':
+        case 1:
             if (computerSelection === 'rock') {
                 return 'You win!';
             } else if (computerSelection === 'paper') {
@@ -18,7 +18,7 @@ function playRound(playerSelection, computerSelection) {
                 return 'You lose!';
             }
             break
-        case '3':
+        case 2:
             if (computerSelection === 'rock') {
                 return 'You lose!';
             } else if (computerSelection === 'paper') {
@@ -31,19 +31,37 @@ function playRound(playerSelection, computerSelection) {
             break;
     }
 }
-
 function computerPlay() {
-    let options = ['rock', 'paper', 'scissors'];
     let num = Math.floor(Math.random() * 3);
     return options[num];
 }
+function game() {
+    while (count < 5) {
+        console.log(`Round ${count}`);
+        playerSelection = prompt('1 - Rock, 2 - Paper, 3 - Scissors:');
 
-let playerSelection = '';
-while (true) {
-    playerSelection = prompt('1 - Rock, 2 - Paper, 3 - Scissors: ', 1);
-    if (playerSelection === 'n') break;
-    if (playerSelection < 0 || playerSelection > 3) continue;
-    let computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
+        if (playerSelection === 'n') {
+            console.log('Bye!');
+            break;
+        };
+
+        playerSelection = parseInt(playerSelection - 1);
+
+        if (playerSelection < 0 || playerSelection > 2) {
+            console.log('Choose a number in the range of 1-3!');
+            continue;
+        };
+
+        let computerSelection = computerPlay();
+        console.log(`${options[playerSelection]} vs ${computerSelection}!`);
+        console.log(playRound(playerSelection, computerSelection));
+        count++;
+    };
 };
-console.log('Bye!');
+let count = 1;
+let playerSelection = '';
+const options = ['rock', 'paper', 'scissors'];
+
+
+
+game();
