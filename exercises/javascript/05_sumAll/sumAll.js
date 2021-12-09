1dde50672047f5c87b903150042b05196d7fe776
root <root@d3bian.cyan>
2021-12-09 00:02:45 -0500

    finish javascript exercises

--- exercises/javascript/05_sumAll/sumAll.js
+++ exercises/javascript/05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    let sum = 0;
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
+    if (num1 < num2) {
+        for (num1; num1 <= num2; num1++) {
+            sum += num1;
+        }
+    } else {
+        for (num2; num2 <= num1; num2++) {
+            sum += num2;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
